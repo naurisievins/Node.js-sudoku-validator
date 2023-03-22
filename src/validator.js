6fe7c6488f3dd7640fd8b43aa15656b93a78bb0f
naurisievins <nauris.ievins@gmail.com>
2023-03-22 15:46:42 +0200

    Improved code, added more comments

--- src/validator.js
+++ src/validator.js
@@ -1,21 +1,52 @@
 class Validator {
   static validate(sudoku) {
-    const validator = new Validator
+    const validator = new Validator();
+    const sudokuNumberArray = validator.getSudokuNumberArray(sudoku);
 
-    return validator.validate(sudoku)
+    return validator.validate(sudokuNumberArray)
   }
 
-  validate(sudoku) {
 
-    // Get array of all sudoku numbers.
+  // Get array of all sudoku numbers.
+  getSudokuNumberArray(sudoku) {
     const sudokuNumberArray = sudoku
-      .replaceAll(/[-+|]/g, '')
-      .replaceAll(/(\n)/g, ' ')
-      .replaceAll('  ', ' ')
+      .replaceAll(/[-+|]/g, '') // Replace all '-', '+', '|' to ''.
+      .replaceAll(/(\n\n|\n)/g, ' ') // Replace all '\n' or '\n\n' to ' '.
       .trim()
       .split(' ')
-      .map(Number);
+      .map(Number); // Converts each array element to number.
 
+    return sudokuNumberArray;
+  }
+
+
+  // Check for duplicate numbers in sudoku.
+  checkDuplicateNumbers(numberArray) { // For sudokuRows, sudokuColumns, sudokuSquares.
+    let filteredArray, numberSet;
+    let hasDuplicates = false;
+
+    numberArray.forEach(array => {
+      filteredArray = array.filter(number => number !== 0); // Filter out zeros.
+      numberSet = new Set(filteredArray); // Set of unique numbers.
+
+      if (filteredArray.length !== numberSet.size) { // Sets hasDuplicates to true if array contained duplicate numbers.
+        hasDuplicates = true;
+      }
+    })
+    
+    return hasDuplicates;
+  }
+
+
+  // Check for zeros in sudoku.
+  checkZeros(sudokuNumberArray) {
+    if (sudokuNumberArray.includes(0)) {
+      return true
+    }
+  }
+
+  // Validate sudoku.
+  validate(sudokuNumberArray) {
 
     // Test if only numbers 1 - 9 are used to fill the puzzle (or 0 for empty cells).
     const isInRange = sudokuNumberArray.every((number) => number >= 0 && number < 10);
@@ -31,38 +62,15 @@ class Validator {
     sudokuNumberArray.forEach((number) => {
       row.push(number);
 
-      if (row.length === 9) {
+      if (row.length === 9) { // 9 for numbers in row.
         sudokuRows.push(row);
         row = [];
       }
     })
 
 
-    // Function to check for duplicate numbers or zeros in sudoku.
-    let hasZero = false;
-
-    const checkDuplicateNumbers = (numberArray) => { // For sudokuRows, sudokuColumns, sudokuSquares.
-      let filteredArray, numberSet, notValid;
-
-      numberArray.forEach(array => {
-        filteredArray = array.filter(number => number !== 0); // Filter out zeros.
-        numberSet = new Set(filteredArray); // Set of unique numbers.
-
-        if (filteredArray.length !== array.length) { // Sets hasZero to true if array contained zeros.
-          hasZero = true;
-        }
-
-        if (filteredArray.length !== numberSet.size) { // Sets notValid to true if array contained duplicate numbers.
-          notValid = true;
-        }
-      })
-      
-      return notValid;
-    }
-
-
     // Check for duplicate numbers in rows.
-    if (checkDuplicateNumbers(sudokuRows)) {
+    if (this.checkDuplicateNumbers(sudokuRows)) {
       return `Sudoku is invalid.`;
     }
 
@@ -71,7 +79,7 @@ class Validator {
     const sudokuColumns = [];
     let col = [];
 
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) { // 9 for numbers in column.
       sudokuRows.forEach(row => col.push(row[i]))
       sudokuColumns.push(col);
       col = [];
@@ -79,7 +87,7 @@ class Validator {
 
 
     // Check for duplicate numbers in columns.
-    if (checkDuplicateNumbers(sudokuColumns)) {
+    if (this.checkDuplicateNumbers(sudokuColumns)) {
       return `Sudoku is invalid.`;
     }
 
@@ -90,14 +98,14 @@ class Validator {
     let square1 = [];
     let square2 = [];
 
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) { // 9 for rows in sudoku.
       for (let j = 0; j < 3; j++) {
-        square.push(sudokuRows[i][j]);
-        square1.push(sudokuRows[i][j+3]);
-        square2.push(sudokuRows[i][j+6]);
+        square.push(sudokuRows[i][j]); // Iterating through row gets first 3 of 9 numbers of row.
+        square1.push(sudokuRows[i][j+3]); // Iterating through row gets middle 3 of 9 numbers of row.
+        square2.push(sudokuRows[i][j+6]); // Iterating through row gets last 3 of 9 numbers of row.
       }
 
-      if(square.length === 9) {
+      if(square.length === 9) { // 9 for numbers in square.
         sudokuSquares.push(square, square1, square2);
         square = [];
         square1 = [];
@@ -107,14 +115,14 @@ class Validator {
 
 
     // Check for duplicate numbers in squares.
-    if (checkDuplicateNumbers(sudokuSquares)) {
+    if (this.checkDuplicateNumbers(sudokuSquares)) {
       return `Sudoku is invalid.`;
     }
 
 
     // No duplicate or invalid numbers - sudoku is valid.
     // Check for zeros in sudoku.
-    if (hasZero) {
+    if (this.checkZeros(sudokuNumberArray)) {
       return `Sudoku is valid but incomplete.`;
     }
     
